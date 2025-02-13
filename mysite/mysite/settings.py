9be90196d7f973d789e01c248246d013142e8768
Tykhon Byshkin <tykhonbyshkin07@gmail.com>
2025-02-13 14:50:13 -0500

    fix settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,7 +107,7 @@ DATABASES = {
 
     }
 }
-"""
+
 
 
 
