8443d26f614dd90d0bde7187c84e10c66bc4695e
SpaceCore912 <tykhonbyshkin07@gmail.com>
2025-04-26 19:14:01 -0400

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&&*ky_8)=+srl&$uabc4a0uc2u649eyx$1ex=@n-&tq)w89o!x"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
